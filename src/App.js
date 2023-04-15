1a81cd996f976b0bf575fe976e6d9bffc5bb22eb
Olowokere Destiny <destinyolowokere@gmail.com>
2023-04-15 11:36:45 +0100

    First commit from project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Box from "./box"
+import { v4 as uuidv4 } from 'uuid';
+import Confetti from "react-confetti"
 
 function App() {
+
+  const [dice, setDice] = useState(newRoll());
+
+  const [tenzies, setTenzies] = useState(false);
+
+  
+  function newDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: uuidv4()
+    }
+    
+  }
+  
+  function newRoll() {
+    const arr = [];
+    for (let x = 0; x < 10; x++) {
+      arr.push(newDie());
+    }
+    return arr;
+  }
+  
+  function holdDice(id) {
+    setDice(oldDie => oldDie.map(
+      die => {
+        if (die.id === id) {
+          return {...die, isHeld: !die.isHeld}
+        } else {
+          return die
+        }
+      }
+    ))
+  }
+
+  function rollDice() {
+    setDice(oldDie => oldDie.map(
+      die => {
+       return die.isHeld ? die : newDie()
+      }
+    ))
+  }
+
+  useEffect(() => {
+    const allHeld = dice.every(die => {
+      return die.isHeld
+    });
+    const sameValue = dice.every(die => {
+      return die.value === dice[0].value
+    })
+
+    allHeld && sameValue ? setTenzies(true) : setTenzies(false)
+  }, [dice])
+
+  const styling = {color: "#444"}
+
+  const diceElements = dice.map(die => {
+  return <Box 
+            number={die.value}
+            key={die.id}
+            class={die.isHeld ? "held" : ""}
+            holdDice={() => holdDice(die.id)}
+          />
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="box-container">
+        {diceElements}
+      </div>
+
+      <button onClick={rollDice}>{tenzies ? "You won!" : "Roll"}</button>
+      {tenzies && <p style={styling}>Refresh browser to play again...</p>}
+      {tenzies && <Confetti />}
+
     </div>
   );
 }
